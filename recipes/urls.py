d6dae4df9253e82d3a1402bb3b3f557b9207af4e
G15 <g.n.d2014@gmail.com>
2023-12-25 20:38:03 -0300

    Removido contato/sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,8 +3,6 @@ from recipes import views
 
 
 urlpatterns = [
-    path('outra-coisa-qualquer/', views.my_view),
     path('', views.home),  # Home
-    path('sobre/', views.sobre),  # /sobre/
-    path('contato/', views.contato),  # /contato/
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,17 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def my_view(request):
-    return HttpResponse('UM LINDA STRING')
-
-
 def home(request):
     return render(request, 'recipes/home.html', context={'name': 'Gabriel Nathan'},)
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
