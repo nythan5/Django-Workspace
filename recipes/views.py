167fc75484074520dda0b20083ffdd1624e7a907
G15 <g.n.d2014@gmail.com>
2023-12-25 21:50:17 -0300

    Finalizando a aula 28

--- recipes/views.py
+++ recipes/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'Gabriel Nathan'},)
+    return render(request, 'recipes/pages/home.html', context={'name': 'Gabriel Nathan'},)
