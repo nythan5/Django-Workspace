1808a53fff1659a50474b3f38d076f7e5e819390
nythan5 <g.n.d2014@gmail.com>
2024-01-02 14:12:13 -0300

    Finalizado aula 55

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', include('recipes.urls'))
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
