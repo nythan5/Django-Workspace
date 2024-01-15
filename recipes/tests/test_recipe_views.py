9f261ec6df2a25f680918ab6cd2804bc80957d7d
nythan5 <g.n.d2014@gmail.com>
2024-01-15 16:43:28 -0300

    .

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -39,7 +39,18 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
 
-        pass
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Check if one recipe exists
+        self.assertIn(
+            'No recipes found here',
+            response.content.decode('utf-8')
+        )
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
@@ -63,6 +74,17 @@ class RecipeViewsTest(RecipeTestBase):
 
         # Check if one recipe exists
         self.assertIn(needed_title, content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+
+        # Check if one category exists
+        self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
@@ -94,3 +116,14 @@ class RecipeViewsTest(RecipeTestBase):
 
         # Check if one recipe exists
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': recipe.id}))
+
+        # Check if one category exists
+        self.assertEqual(response.status_code, 404)
