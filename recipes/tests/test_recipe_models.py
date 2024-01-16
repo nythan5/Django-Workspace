4b440ca807e4e7c376fc805345a3e364184b5543
nythan5 <g.n.d2014@gmail.com>
2024-01-15 22:08:17 -0300

    Finalizado aula 92 necessario instalar o parameterized

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,4 +1,6 @@
 from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 
 class RecipeModelTest(RecipeTestBase):
@@ -6,6 +8,14 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()  # Aqui a validacao ocorre
