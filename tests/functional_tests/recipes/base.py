c77d772053a4e82d427444fe1dd683065a678471
nythan5 <g.n.d2014@gmail.com>
2024-02-22 11:48:11 -0300

    Finalizado aula 167

---
+++ tests/functional_tests/recipes/base.py
@@ -0,0 +1,16 @@
+from utils.browser import make_chrome_browser
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import time
+
+
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
