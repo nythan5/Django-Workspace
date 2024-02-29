2e9e40be35b0cd41c22e82035b059f06be4939ef
nythan5 <g.n.d2014@gmail.com>
2024-02-29 09:30:53 -0300

    Finalizada aula 190

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -12,7 +12,7 @@ class AuthorRecipeForm(forms.ModelForm):
         add_attr(self.fields.get('cover'), 'class', 'span-2')
 
     class Meta:
-        model = Recipe()
+        model = Recipe
         fields = ('title', 'description', 'preparation_time',
                   'preparation_time_unit', 'servings', 'servings_unit',
                   'preparation_steps', 'cover')
