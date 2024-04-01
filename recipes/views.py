4ef24405187daf482927d91e42251a237fb21e0d
nythan5 <g.n.d2014@gmail.com>
2024-04-01 16:08:14 -0300

    Finalizando aula 230

--- recipes/views.py
+++ recipes/views.py
@@ -25,6 +25,9 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(
             is_published=True,
         )
+
+        qs = qs.select_related('author', 'category')
+
         return qs
 
     def get_context_data(self, *args, **kwargs):
