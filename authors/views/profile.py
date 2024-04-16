a832414ca3631e6169f205c230543659da7f8fe6
nythan5 <g.n.d2014@gmail.com>
2024-04-16 10:35:19 -0300

    Finalizando aula 262

---
+++ authors/views/profile.py
@@ -0,0 +1,17 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from authors.models import Profile
+
+
+class ProfileView(TemplateView):
+    template_name = 'authors/pages/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        profile_id = context.get('id')
+        profile = get_object_or_404(
+            Profile.objects.filter(pk=profile_id).select_related('author'), pk=profile_id)  # noqa
+        return self.render_to_response({**context, 'profile': profile})
